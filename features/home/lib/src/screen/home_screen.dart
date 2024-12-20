315c3d97095324e5198f37332ad247f5c5621521
German Perelmuter <kysbeg@gmail.com>
2024-12-20 15:50:43 +0100

    Merge feature/initialize into dev

---
+++ features/home/lib/src/screen/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:core/core.dart';
+import 'package:flutter/material.dart';
+import 'package:navigation/navigation.dart';
+
+@RoutePage()
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
