8afcbe90ca2e9d4d3fe3ca20ecb6e849efc04624
German Perelmuter <kysbeg@gmail.com>
2024-12-20 15:50:43 +0100

    initialize project using DevPilot

--- features/home/lib/home.dart
+++ features/home/lib/home.dart
@@ -1,11 +1,16 @@
 library home;
 
 import 'package:navigation/navigation.dart';
-
 export 'home.gr.dart';
 
-@AutoRouterConfig()
+@AutoRouterConfig(replaceInRouteName: 'Page|Screen|View,Route')
 class HomeRouter extends RootStackRouter {
   @override
-  List<AutoRoute> get routes => <AutoRoute>[];
+  List<AutoRoute> get routes => <AutoRoute>[
+    AutoRoute(
+      initial: true,
+      page: HomeRoute.page,
+      path: RoutePath.home,
+    )
+  ];
 }
--- navigation/lib/navigation.dart
+++ navigation/lib/navigation.dart
@@ -2,6 +2,6 @@ library navigation;
 
 export 'package:auto_route/auto_route.dart';
 export 'package:home/home.dart';
-
 export 'src/app_router/app_router.dart';
 export 'src/di/navigation_di.dart';
+export 'src/route_path.dart';
